f5df6e2301854d5ac0b890445b270a4182079a65
Anatolii Sliusariev <anatolii.sliusariev@gmail.com>
2026-01-31 12:47:58 +0100

    fix

--- game.js
+++ game.js
@@ -394,7 +394,7 @@ function playWinSfx(name) {
 
   winnerPresound.pause();
   winnerPresound.currentTime = 0;
-  winnerPresound.volume = 0.1;
+  winnerPresound.volume = 0.06;
   winnerPresound.play().catch(() => {});
   winnerSound.play().catch(() => {});
 }
